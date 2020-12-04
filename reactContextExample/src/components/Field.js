e3a7542bccf576b95a602962194a4c0608c0685a
Mila Ashtari <mashtari1991@gmail.com>
2020-12-04 13:19:15 -0500

    README file added

--- reactContextExample/src/components/Field.js
+++ reactContextExample/src/components/Field.js
@@ -2,9 +2,11 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+  // The line below is how we link a class component to the context object
   static contextType = LanguageContext;
 
   render() {
+    // the information inside the context object is refrenced by this.context
     const text = this.context === 'english' ? 'Name' : 'Naam';
 
     return (
--- reactContextExample/src/contexts/LanguageContext.js
+++ reactContextExample/src/contexts/LanguageContext.js
@@ -1,3 +1,3 @@
 import React from 'react';
-
+// default value of english is passed to the LanguageContext 
 export default React.createContext('english');
